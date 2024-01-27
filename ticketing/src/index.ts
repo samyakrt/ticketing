52273c10a8d7c28b550f4824219438bdcf9c2cc7
samyak <tuladharsamyak@gmail.com>
2024-01-27 17:05:37 +0545

    ticketing setup

---
+++ ticketing/src/index.ts
@@ -0,0 +1,21 @@
+import 'express-async-errors';
+import express from 'express';
+import mongoose from 'mongoose';
+import cookieSession from 'cookie-session';
+import app from './app';
+
+const startApp = async () => {
+    try {
+        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+            autoIndex: true,
+        })
+        console.log('Connected to mongodb');
+
+        app.listen(3000,() => console.log('listening to port 3000'))
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+startApp()
+
