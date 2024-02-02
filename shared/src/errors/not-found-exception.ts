982b203a5fd69e841e672d302ff0295ca4223acb
samyak <tuladharsamyak@gmail.com>
2024-02-02 07:00:15 +0545

    status code updated

--- shared/src/errors/not-found-exception.ts
+++ shared/src/errors/not-found-exception.ts
@@ -2,7 +2,7 @@ import { CustomException } from "./custom-exception";
 import httpStatusCodes from 'http-status-codes';
 
 export class NotFoundException extends CustomException {
-    public readonly httpStatus = httpStatusCodes.BAD_REQUEST;
+    public readonly httpStatus = httpStatusCodes.NOT_FOUND;
     
     constructor(message = 'URL not found'){
         super(message);
