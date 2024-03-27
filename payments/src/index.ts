64a48e0d316fb7d476f6dcd8b68b6fd7bab36861
samyak <tuladharsamyak@gmail.com>
2024-03-28 05:38:39 +0545

    inital setup

---
+++ payments/src/index.ts
@@ -0,0 +1,37 @@
+import 'express-async-errors';
+import mongoose from 'mongoose';
+import app from './app';
+import { natsWrapper } from '@/nats-wrapper';
+import env from './env';
+
+const startApp = async () => {
+    try {
+        await mongoose.connect(env.MONGODB_URI, {
+            autoIndex: true,
+        })
+
+        await natsWrapper.connect(env.NATS_CLUSTER_ID, env.NATS_CLIENT_ID, env.NATS_URL);
+        natsWrapper.client.on('close', () => {
+            console.log('nats closed')
+            process.exit()
+        })
+
+
+        console.log('Connected to mongodb');
+
+        app.listen(3000, () => console.log('listening to port 3000'))
+
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+startApp()
+process.on('uncaughtException', (error: Error) => {
+    console.error(`Caught exception: ${error}\n` + `Exception origin: ${error.stack}`);
+    natsWrapper.client.close();
+    process.exit(1);
+});
+process.on('SIGINT', () => natsWrapper.client.close())
+process.on('SIGTERM', () => natsWrapper.client.close())
+
